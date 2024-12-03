f62769a41c725ebd0b3366f65943038035c17cca
agubelu <agustinbd3@gmail.com>
2024-12-03 10:29:32 +0000

    💾 Day 03 💾

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -9,16 +9,16 @@ pub fn solve() -> SolutionPair {
     let re = Regex::new(r#"(do|don't|mul)\((\d+,\d+)?\)"#).unwrap();
 
     let (mut sol1, mut sol2) = (0, 0);
-    let mut enabled = true;
+    let mut enabled = 1;
 
     for cap in re.captures_iter(&input) {
         match &cap[1] {
-            "do" => enabled = true,
-            "don't" => enabled = false,
+            "do" => enabled = 1,
+            "don't" => enabled = 0,
             "mul" => {
                 let mult: u64 = cap[2].split(',').map(|x| x.parse::<u64>().unwrap()).product();
                 sol1 += mult;
-                sol2 += mult * enabled as u64;
+                sol2 += mult * enabled;
             },
             _ => unreachable!(),
         }
