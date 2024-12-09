dca321112ba68c626a5df761f96481ce23a6d213
agubelu <agustinbd3@gmail.com>
2024-12-09 19:03:11 +0000

    🖴 Day 09 🖴

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -4,6 +4,8 @@ use std::cmp::Ordering::*;
 
 ///////////////////////////////////////////////////////////////////////////////
 
+const SPACE: i64 = -1;
+
 #[derive(Copy, Clone, Debug)]
 struct Block {
     size: i64,
@@ -27,8 +29,8 @@ fn compact_v1(layout: &[Block]) -> i64 {
 
     while file_ptr > space_ptr {
         move_block(&mut layout, file_ptr, space_ptr);
-        while layout[space_ptr].ident != -1 { space_ptr += 1 }
-        while layout[file_ptr].ident == -1 { file_ptr -= 1 }
+        while layout[space_ptr].ident != SPACE { space_ptr += 1 }
+        while layout[file_ptr].ident == SPACE { file_ptr -= 1 }
     }
 
     checksum(&layout)
@@ -47,11 +49,11 @@ fn compact_v2(mut layout: Vec<Block>) -> i64 {
         target_ident -= 1;
 
         // Find the first space where this block can fit
-        let space = (0..file_ptr).find(|&i| layout[i].ident == -1 && layout[i].size >= block_to_move.size);
+        let space = (0..file_ptr).find(|&i| layout[i].ident == SPACE && layout[i].size >= block_to_move.size);
         if let Some(space_ix) = space {
             move_block(&mut layout, file_ptr, space_ix);
 
-            if layout[space_ix + 1].ident == -1 {
+            if layout[space_ix + 1].ident == SPACE {
                 file_ptr += 1; // A new space has been added
             }
         }
@@ -63,7 +65,7 @@ fn compact_v2(mut layout: Vec<Block>) -> i64 {
 fn checksum(layout: &[Block]) -> i64 {
     let (mut sum, mut pos) = (0, 0);
     for b in layout {
-        if b.ident != -1 {
+        if b.ident != SPACE {
             let (start, end) = (pos, pos + b.size - 1);
             sum += (end - start + 1) * (end + start) / 2 * b.ident; // Sum of the interval [start, end] * ident
         }
@@ -83,8 +85,8 @@ fn move_block(layout: &mut Vec<Block>, source_ix: usize, target_ix: usize) {
             // The block partially fills the space; a new space is added
             let leftover_space = space_size - file_size;
             layout[target_ix] = layout[source_ix];
-            layout[source_ix] = Block{ size: file_size, ident: -1 };
-            layout.insert(target_ix + 1, Block{ size: leftover_space, ident: -1 });
+            layout[source_ix] = Block{ size: file_size, ident: SPACE };
+            layout.insert(target_ix + 1, Block{ size: leftover_space, ident: SPACE });
         },
         Less => {
             // The block is bigger than the space; a leftover remains in the block
@@ -100,7 +102,7 @@ fn parse_layout(input: &str) -> Vec<Block> {
     input.trim_end().char_indices()
          .map(|(i, ch)| {
              let size = ch.to_digit(10).unwrap() as i64;
-             let ident = if i % 2 == 0 { i as i64 / 2 } else { -1 };
+             let ident = if i % 2 == 0 { i as i64 / 2 } else { SPACE };
              Block { size, ident }
          }).collect()
 }
