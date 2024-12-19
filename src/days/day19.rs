1b99ed62440a812ee1a3dbb726add2986a26e5ac
agubelu <agustinbd3@gmail.com>
2024-12-19 11:14:48 +0100

    🧣 Day 19 🧣

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -1,13 +1,46 @@
-use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
+
+use itertools::Itertools;
+use rayon::prelude::*;
+use rustc_hash::FxHashMap;
+
+use crate::etc::DOUBLE_NEWLINE;
+use crate::{Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
-pub fn solve() -> SolutionPair {
-    let _input = read_to_string("input/day01.txt").unwrap();
+type Cache<'a> = FxHashMap<&'a str, u64>;
 
-    let sol1 = 0u64;
-    let sol2 = 0u64;
+pub fn solve() -> SolutionPair {
+    let input = read_to_string("input/day19.txt").unwrap();
+    let (top, bottom) = input.split_once(DOUBLE_NEWLINE).unwrap();
+
+    let patterns = top.split(", ").collect_vec();
+    let designs = bottom.lines().collect_vec();
+
+    let solved_designs: Vec<u64> = designs.par_iter()
+        .map(|d| solve_design(d, &patterns, &mut Cache::default()))
+        .collect();
+
+    let sol1 = solved_designs.iter().filter(|&c| *c > 0).count();
+    let sol2: u64 = solved_designs.iter().sum();
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn solve_design<'a>(design: &'a str, patterns: &[&'a str], cache: &mut Cache<'a>) -> u64 {
+    if design.is_empty() {
+        return 1; // Design solved
+    }
+    if let Some(&res) = cache.get(design) {
+        return res; // Cache hit
+    }
+
+    let combos: u64 = patterns.iter().copied()
+        .filter(|&pat| pat.len() <= design.len() && &design[..pat.len()] == pat)
+        .map(|pat| solve_design(&design[pat.len()..], patterns, cache))
+        .sum();
+
+    cache.insert(design, combos);
+    combos
 }
